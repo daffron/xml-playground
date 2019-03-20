70925863a342c8f102f4b8b1f9570f380a65a400
Jack Daffron <jack@daffron.co>
2019-03-20 11:46:11 +1030

    Switch libs to keep native

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { xsltProcess, xmlParse } = require('xslt-processor')
+const parser = require('xml-js')
 
 const { input, output, xsl } = require('./file-config.json')
 
@@ -12,6 +13,44 @@ const outXmlString = xsltProcess(
 )
 
 console.log('INPUT: ', inputString)
-console.log('OUTPUT: ', outXmlString)
+console.log('XMl OUTPUT: ', outXmlString)
 
-fs.writeFileSync(output, outXmlString)
+function nativeType(value) {
+  const nValue = Number(value)
+  if (!isNaN(nValue)) {
+    return nValue
+  }
+  const bValue = value.toLowerCase()
+  if (bValue === 'true') {
+    return true
+  } else if (bValue === 'false') {
+    return false
+  }
+  return value
+}
+
+const removeJsonTextAttribute = function(value, parentElement) {
+  try {
+    const keyNo = Object.keys(parentElement._parent).length
+    const keyName = Object.keys(parentElement._parent)[keyNo - 1]
+    parentElement._parent[keyName] = nativeType(value)
+  } catch (e) {}
+}
+
+const options = {
+  compact: true,
+  trim: true,
+  ignoreDeclaration: true,
+  ignoreInstruction: true,
+  ignoreAttributes: true,
+  ignoreComment: true,
+  ignoreCdata: true,
+  ignoreDoctype: true,
+  textFn: removeJsonTextAttribute
+}
+
+const json = JSON.parse(parser.xml2json(outXmlString, options))
+
+console.log('OUTPUT: ', json)
+
+fs.writeFileSync(output, JSON.stringify(json, null, 2))
--- xsl-config/test.xsl
+++ xsl-config/test.xsl
@@ -1,23 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
 
-  <xsl:output method="text" indent="no"/>
+  <xsl:output method="xml" indent="yes"/>
 
 
   <xsl:template match="/">
-        <!-- <xsl:value-of select="concat('test', ',')"/> -->
-    <xsl:apply-templates select="./descendant::*[name() = 'DataSet']"/>
+    <db>
+      <xsl:apply-templates select="//DataSet"/>
+    </db>
   </xsl:template>
 
   <xsl:template match="DataSet">
     <xsl:variable name="dbname">
-        <xsl:value-of select="concat('test', ',')"/>
+        <xsl:value-of select="//@db"/>
     </xsl:variable>
+    
+    <xsl:element name="{$dbname}">
+      <xsl:variable name="db_desc">
+        <xsl:value-of select="//@description"/>
+      </xsl:variable>
+      <description>
+        <xsl:value-of select="$db_desc"/>
+      </description>
+    </xsl:element>
+
   </xsl:template>
-
-  <xsl:variable name="db_desc">
-    <xsl:value-of select="//@description"/>
-  </xsl:variable>
-
 
 </xsl:stylesheet>
